8b8334643c552f18904c018630175959f95fd95e
antoniomattar <antoniomattar123@icloud.com>
2023-05-06 10:50:51 +0200

    done

--- src/components/Round.tsx
+++ src/components/Round.tsx
@@ -26,7 +26,7 @@ export default async function Round() {
       <h1 className="m-6 flex justify-center font-mono text-6xl font-bold ">
         GAMES
       </h1>
-      <div className=" mx-auto max-h-fit max-w-fit justify-center rounded-3xl bg-lime-400 p-8 font-semibold shadow-lg outline-dashed outline-black dark:bg-slate-800">
+      <div className=" mx-auto max-h-fit max-w-fit justify-center rounded-3xl bg-lime-400 p-8 font-semibold shadow-lg outline outline-black dark:bg-slate-800">
         {currentRound['name']}
       </div>
       {/* @ts-expect-error Server Component */}
