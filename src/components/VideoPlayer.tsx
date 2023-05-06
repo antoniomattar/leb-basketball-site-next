3e0d75c7ec447524ac08dc33db80793f1220a49b
antoniomattar <antoniomattar123@icloud.com>
2023-05-06 10:46:54 +0200

    done

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -28,7 +28,11 @@ export default function VideoPlayer() {
     <div>
       <h1 className=" text-center font-mono text-3xl font-bold">Live Stream</h1>
       <div className=" m-6 mx-auto flex w-10/12 justify-center rounded-md border-4 border-green-700 md:w-6/12">
-        <video id="video-player" controls></video>
+        <video
+          id="video-player"
+          controls
+          poster="https://scontent.cdninstagram.com/v/t51.2885-15/344583261_1616376318879400_3493646322755263099_n.jpg?stp=dst-jpg_e35_p1080x1080&_nc_ht=scontent.cdninstagram.com&_nc_cat=104&_nc_ohc=H6w7-mOVGsEAX_Q2kWg&edm=APs17CUBAAAA&ccb=7-5&oh=00_AfCqhdNJvdCAoVRs02QObFIRGfaGCjGEBFMqSAtolEH12g&oe=645B0239&_nc_sid=978cb9&dl=1"
+        ></video>
       </div>
     </div>
   );
