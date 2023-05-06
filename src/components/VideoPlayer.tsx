d3ed809d242b47ca33e178208499e608a3401423
antoniomattar <antoniomattar123@icloud.com>
2023-05-06 10:27:44 +0200

    fixed it

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -5,19 +5,31 @@ import Hls from 'hls.js';
 
 export default function VideoPlayer() {
   useEffect(() => {
-    if (Hls.isSupported()) {
-      const video: any = document.getElementById('video-player');
-      const hls = new Hls();
-      hls.loadSource(
-        'https://1333578254.rsc.cdn77.org/1537084837/tracks-v2a1/mono.m3u8'
-      );
-      hls.attachMedia(video);
+    const userAgent = window.navigator.userAgent.toLowerCase();
+    const isIos = /iphone|ipad|ipod/.test(userAgent);
+    const video: any = document.getElementById('video-player');
+    if (isIos) {
+      video.src =
+        'https://1333578254.rsc.cdn77.org/1537084837/tracks-v2a1/mono.m3u8';
+    } else {
+      if (Hls.isSupported()) {
+        const hls = new Hls();
+        hls.loadSource(
+          'https://1333578254.rsc.cdn77.org/1537084837/tracks-v2a1/mono.m3u8'
+        );
+        hls.attachMedia(video);
+      } else {
+        console.log('Hls.js is not supported');
+      }
     }
   }, []);
 
   return (
-    <div className=" m-6 mx-auto w-6/12 rounded-sm border-2 border-red-500">
-      <video id="video-player" controls></video>
+    <div>
+      <h1 className="  text-center text-3xl font-bold">Live Stream</h1>
+      <div className=" m-6 mx-auto flex w-6/12 justify-center rounded-sm border-2 border-red-500">
+        <video id="video-player" controls></video>
+      </div>
     </div>
   );
 }
