4109a07266567ed37ce15758c40a75e0f6de7bd2
antoniomattar <antoniomattar123@icloud.com>
2023-09-25 18:24:43 +0200

    gogogo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,9 +12,9 @@ export default async function page() {
       <Divider />
       <h1 className=" text-center font-mono text-3xl font-bold">
         {' '}
-        FIBA WORLD CUP #YallaLebnen 🇱🇧
+        DOHA TOURNAMENT 🇱🇧
       </h1>
-      <VideoPlayer videoUrl="https://ed-t1.edghst.me/plyvivo/ve1akefor0xaku4oxa5a/chunklist.m3u8" />
+      <VideoPlayer videoUrl="https://cllive.itworkscdn.net/lbcgrouplive/token=nva=1695668081~dirs=1~hash=0f68654f6425a437d9c3a/lbclive.smil/lbc_360p_chunks.m3u8" />
       {/* @ts-expect-error Server Component */}
       <Standings />
       <Divider />
