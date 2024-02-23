c3425c323c0a282c4fe5f439d4bd25ccec9aab2a
Antonio Mattar <93141410+antoniomattar@users.noreply.github.com>
2024-02-23 16:22:23 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default async function page() {
       <Divider />
       <h1 className=" text-center font-mono text-3xl font-bold">
         {' '}
-        WASL TOURNAMENT 🇱🇧
+        ASIA QUALI  🇱🇧
       </h1>
       <VideoPlayer videoUrl="https://cdn1.ipslow.com/tv525_www.elahmad.com_lb2/index.m3u8?token=00c97a0f2823d40444940db0b144757438024216-baf1b1623ca4efe9c966a7666af43a23-1708712015-1708701215 " />
       {/* @ts-expect-error Server Component */}
