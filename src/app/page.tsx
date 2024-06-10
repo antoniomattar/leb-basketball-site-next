c10020cf4570e7d2f62a17b603b53b51beff295d
antoniomattar <antoniomattar123@icloud.com>
2024-06-10 19:22:08 +0200

    last update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default async function page() {
         {' '}
         ASIA QUALI TOURNAMENT 🇱🇧
       </h1>
-      <VideoPlayer videoUrl="https://fonz02.dlive.click/8QBn2SRD4-DHZZ5Knp8Wzw/1718067915/34362e3139332e35362e323030/ff416/playlist.m3u8" />
+      <VideoPlayer videoUrl="https://fonz01.dlive.click/oMD8AMeqAM8TvRu9JvVNkg/1718068902/34362e3139332e35362e323030/ff416/playlist.m3u8" />
       {/* @ts-expect-error Server Component */}
       <Standings />
       <Divider />
